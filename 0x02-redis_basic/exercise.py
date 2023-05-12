23d0cc8ba577d4ef60c1cea7607c660b2c58426f
jojobutercup <jojobutercup@gmail.com>
2023-05-12 11:19:09 -0700

    commit

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python3
 
-from typing import Callable
+class Cache:
+    def __init__(self):
+        self.calls = []
 
-def replay(func: Callable) -> None:
-    """Replay the history of calls to a function."""
-    # Retrieve the keys for the input and output lists
-    input_key = f"{func.__qualname__}:inputs"
-    output_key = f"{func.__qualname__}:outputs"
-    # Get the input and output lists from Redis
-    inputs = cache._redis.lrange(input_key, 0, -1)
-    outputs = cache._redis.lrange(output_key, 0, -1)
-    # Zip the inputs and outputs together
-    calls = list(zip(inputs, outputs))
-    # Print the function name and number of calls
-    print(f"{func.__qualname__} was called {len(calls)} times:")
-    # Loop over the calls and print each one
-    for i, (input_str, output_str) in enumerate(calls):
-        input_args = eval(input_str)  # Convert the input string to a tuple
-        output = output_str.decode()  # Convert the output bytes to a string
-        print(f"{func.__qualname__}{input_args} -> {output}")
+    def store(self, value):
+        call = (value, str(uuid.uuid4()))
+        self.calls.append(call)
+        return call[1]
+
+def replay(func):
+    calls = [call for call in func.calls if call[0] == func.store]
+    print(f"{func.__name__} was called {len(calls)} times:")
+    for call in calls:
+        args_str = ", ".join([repr(arg) for arg in call[:-1]])
+        print(f"{func.__name__}(*({args_str},)) -> {call[-1]}")
+
+cache = Cache()
+cache.store("foo")
+cache.store("bar")
+cache.store("egg")
+cache.store("eggsperiment")
+replay(cache.store)
 
